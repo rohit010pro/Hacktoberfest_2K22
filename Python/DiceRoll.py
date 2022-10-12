3e95490494eec46fbc59c6e104e13dca7a3edc0a
Vedant Sharma <vedant.12sharma@gmail.com>
2022-10-12 18:21:17 +0530

    Added DiceRoll.py

---
+++ Python/DiceRoll.py
@@ -0,0 +1,17 @@
+import random
+    
+min_val = 1
+max_val = 6
+
+#the variable that stores the user’s decision
+roll_again = "yes"
+
+#The dice roll loop if the user wants to continue
+while roll_again == "yes" or roll_again == "y":
+    print("Dices rolling...")
+    print("The values are :")
+    print(random.randint(min_val, max_val))
+    print(random.randint(min_val, max_val))
+
+    #Here the user enters yes or y to continue and any other input ends the program
+    roll_again = input("Roll the Dices Again?")
